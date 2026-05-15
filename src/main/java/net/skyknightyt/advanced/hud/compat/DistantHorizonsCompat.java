11a36143d03ec02f36ce93bf2f02be4b459119d9
SKYKNIGHTYT <sky.knight.finances@gmail.com>
2026-05-16 02:19:48 +0600

    Updated DistantHorizonsCompat.java with new packages and annotations
    - Added @Enviroment
    - Packages are now under net.skyknightyt.advanced.hud

--- src/main/java/net/skyknightyt/advanced/hud/compat/DistantHorizonsCompat.java
+++ src/main/java/net/skyknightyt/advanced/hud/compat/DistantHorizonsCompat.java
@@ -1,7 +1,10 @@
-package net.skylen.advanced.hud.compat;
+package net.skyknightyt.advanced.hud.compat;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.loader.api.FabricLoader;
 
+@Environment(EnvType.CLIENT)
 public class DistantHorizonsCompat {
     public static final boolean DISTANT_HORIZONS = FabricLoader.getInstance().isModLoaded("distanthorizons");
 }
