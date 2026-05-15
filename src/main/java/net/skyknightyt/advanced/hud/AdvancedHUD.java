edd4b0d23da274ac0f2a72180a2bf4a5c38e6c59
SKYKNIGHTYT <sky.knight.finances@gmail.com>
2026-05-16 03:05:54 +0600

    Updated AdvancedHUD.java

--- src/main/java/net/skyknightyt/advanced/hud/AdvancedHUD.java
+++ src/main/java/net/skyknightyt/advanced/hud/AdvancedHUD.java
@@ -1,21 +1,26 @@
-package net.skylen.advanced.hud;
+package net.skyknightyt.advanced.hud;
 
 import net.fabricmc.api.ClientModInitializer;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.VanillaHudElements;
 import net.minecraft.resources.Identifier;
-import net.skylen.advanced.hud.config.AdvancedHUDConfigData;
-import net.skylen.advanced.hud.elements.equipment.EquipmentElementV3;
-import net.skylen.advanced.hud.elements.generic.*;
+import net.skyknightyt.advanced.hud.config.AdvancedHUDConfigData;
+import net.skyknightyt.advanced.hud.config.data.ArmorSettingsData;
+import net.skyknightyt.advanced.hud.elements.equipment.EquipmentElement;
+import net.skyknightyt.advanced.hud.elements.generic.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@Environment(EnvType.CLIENT)
 public class AdvancedHUD implements ClientModInitializer {
 	public static final String ADVANCEDHUD_MOD_ID = "advancedhud";
 	public static final Logger LOGGER = LoggerFactory.getLogger(ADVANCEDHUD_MOD_ID);
 
 	public static AdvancedHUDConfigData CONFIG = new AdvancedHUDConfigData();
+	public static ArmorSettingsData ARMOR_CONFIG = new ArmorSettingsData();
 
 	@Override
 	public void onInitializeClient() {
@@ -34,7 +39,7 @@ public class AdvancedHUD implements ClientModInitializer {
 			HudElementRegistry.attachElementAfter(
 					VanillaHudElements.CHAT,
 					Identifier.fromNamespaceAndPath(ADVANCEDHUD_MOD_ID, "equipment"),
-					EquipmentElementV3::render
+					EquipmentElement::render
 			);
 			HudElementRegistry.attachElementAfter(
 					VanillaHudElements.CHAT,
